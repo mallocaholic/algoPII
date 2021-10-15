24243244dc96430ad785288f98f790e18225f1c0
Junior Marcondes <jrmarcondesb@gmail.com>
2021-10-15 14:28:11 -0300

    f5 lista2

--- lista2.cpp
+++ lista2.cpp
@@ -37,6 +37,7 @@ class _song_hashtable{ // defining hastable class
     private:
     _song *bucket;
     int max_capacity, size;
+    float factor;
     
     public:
     _song_hashtable(int capacity){  // defining constructor to hashtable
@@ -44,11 +45,40 @@ class _song_hashtable{ // defining hastable class
         bucket = new _song[capacity]; // creating buckets
         max_capacity = capacity;
         size = 0;
+        factor = 0.0f;
+        
+    }
+
+    ~_song_hashtable(){
+        delete[] bucket;
+    }
+
+    _song_hashtable *resize(){
+
+        factor = (float) ++size / (float) max_capacity;
+
+        if(factor >= 50){
+            
+            max_capacity = 2*max_capacity + 1;
+            
+            _song_hashtable *new_hash = new _song_hashtable(max_capacity);
+            new_hash->size = size;
+
+            for(int i = 0; i < max_capacity; i++) // coppying data
+                new_hash->bucket[i] = bucket[i];
+
+            this->~_song_hashtable();
+
+            return new_hash;
+
+        } 
+        
+        else return this;
 
     }
 
     void add(std::string _name, int duration){ // adding song to bucket
-        
+
         _song new_song;
         new_song.name = _name;
         new_song.duration = duration;
@@ -62,12 +92,11 @@ class _song_hashtable{ // defining hastable class
         bucket[new_song.key] = new_song;
 
         std::cout << bucket[new_song.key].name << SPACE 
-                  << bucket[new_song.key].duration << SPACE 
                   << new_song.key << endl;
 
     }
 
-    void play(std::string _name, int time){
+    void play(std::string _name){
 
         int _key = key(_name) % max_capacity;
 
@@ -76,7 +105,7 @@ class _song_hashtable{ // defining hastable class
             if(_key > max_capacity) _key = _key % max_capacity;
         }
 
-        bucket[_key].lis_time += time;
+        bucket[_key].lis_time += bucket[_key].duration;
 
         std::cout << bucket[_key].name << SPACE 
                   << bucket[_key].lis_time << endl;
@@ -115,22 +144,27 @@ int main(){
 
             std::cin >> name >> duration;
             hash->add(name, duration);
+            hash->resize();
+
         }
         else if( action == "PLAY" ){
 
-            std::cin >> name >> duration;
-            hash->play(name, duration);
+            std::cin >> name;
+            hash->play(name);
 
         }
         else if( action == "TIME" ){
             
             std::cin >> name;
+            hash->time_listened(name);
 
         }
 
         std::cin >> action;
 
     }
+    
+    delete hash;
     
     return 0;
 }
